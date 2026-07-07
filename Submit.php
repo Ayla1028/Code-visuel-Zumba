38299ab913d71b74d5930421beda091a16192f83
Ayla1028 <Khalife1002@gmail.com>
2026-07-07 20:03:41 +0300

    Create Submit.php

---
+++ Submit.php
@@ -0,0 +1,8 @@
+<?php
+
+echo "Name: " . $_POST["name"] . "<br>";
+echo "Age: " . $_POST["age"] . "<br>";
+echo "Message: " . $_POST["message"] . "<br>";
+echo "Session: " . $_POST["inscription"];
+print_r($_POST);
+?>
